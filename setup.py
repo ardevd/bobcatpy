aefc1f9cf98870076cae245d6f43857c17fe028e
ardevd <edvard.holst@gmail.com>
2022-04-18 18:09:21 +0200

    updated version info for v1.0.5

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="bobcatpy",
-    version="1.0.3",
+    version="1.0.5",
     author="ardevd",
     author_email="5gk633atf@relay.firefox.com",
     description="Control and monitor your Bobcat miner",

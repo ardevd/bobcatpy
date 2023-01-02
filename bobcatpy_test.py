045a37e3d0292bd87be7232c34ec45caca4b771a
ardevd <edvard.holst@gmail.com>
2023-01-02 21:18:42 +0100

    change test ip

--- bobcatpy_test.py
+++ bobcatpy_test.py
@@ -3,7 +3,7 @@ from bobcatpy import Bobcat
 
 class TestParseTemperature(unittest.IsolatedAsyncioTestCase):
     async def test_parse_valid_temperature(self):
-        b = Bobcat("10.10.21.70")
+        b = Bobcat("192.168.10.10")
         temp_string = "12 °C"
         self.assertEqual(b._parse_temperature(temp_string), "12")
         await b.close_session()

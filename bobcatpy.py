05ecfba36f52b2256e9a13e44de3fa6c1bb148cd
ardevd <edvard.holst@gmail.com>
2023-01-06 10:25:38 +0100

    Added custom exceptions, replaced generic exception handling

--- bobcatpy.py
+++ bobcatpy.py
@@ -47,13 +47,13 @@ class Bobcat:
         """Reset the miner"""
         return await self._post("admin/reset", ADMIN_AUTH_HEADER)
     
-    async def _get(self, uri):
+    async def _get(self, uri: str):
         """Generic GET request helper function"""
         url = f'{self.base_url}{uri}'
         async with self.session.get(url) as resp:
             return await resp.json()
 
-    async def _post(self, uri, headers=None):
+    async def _post(self, uri: str, headers=None):
         """Generic POST request helper function"""
         url = f'{self.base_url}{uri}'
         async with self.session.post(url, headers = headers) as resp:
@@ -63,7 +63,7 @@ class Bobcat:
         """Close the connection session"""
         await self.session.close()
     
-    def _parse_temperature(self, temperature_string):
+    def _parse_temperature(self, temperature_string: str):
         """Parse the temperature value from temperature value string"""
         return re.findall(r"\d+", temperature_string)[0]
 
@@ -84,6 +84,12 @@ class Bobcat:
             miner_status = await self.miner_status()
             
             if miner_status:
+                # Check if we get the rate limit exception
+                if "message" in miner_status.keys():
+                    if miner_status["message"] == "rate limit exceeded":
+                    # Rate limit exceeded
+                        raise BobcatRateLimitException("Rate limit exceeded, try again later")
+
                 summary['ota_version'] = miner_status['ota_version']
                 summary['image'] = miner_status['miner']['Image']
                 summary['image_version'] = summary['image'].split(':', 1)[1] if ':' in summary['image'] else None
@@ -95,12 +101,20 @@ class Bobcat:
                 summary['temp'] = self._parse_temperature(miner_status['temp0'])
                 summary['error'] = miner_status['errors'] != ''
 
-        except Exception as ex:
-            logger.error('Exception during status update: %s', ex)
+        except aiohttp.ClientError as ex:
+            logger.error("Failed to connect to host %s", self.miner_ip)
+            raise BobcatConnectionError("Failed to connect to miner. Verify IP and connectivity") from ex
 
         try:
             summary['blockchain_height'] = await self.blockchain_height()
-        except Exception as ex:
-            logger.error('Exception during blockchain height check: %s', ex)
+        except aiohttp.ClientError as ex:
+            logger.error("Failed to obtain blockchain height")
+            raise ex
 
         return summary
+
+class BobcatRateLimitException(Exception):
+    """Bobcat Miner rate limit exceeded"""
+
+class BobcatConnectionError(Exception):
+    """Failed to connect to miner"""
